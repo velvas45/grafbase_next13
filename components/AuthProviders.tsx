5ff65b2beda503ad65fd6977abc337eac692bbb1
helmi.agustiawan <helmi.agustiawan@JKT-MAC-2114.local>
2023-06-26 17:09:09 +0700

    update next auth

--- components/AuthProviders.tsx
+++ components/AuthProviders.tsx
@@ -1,7 +1,42 @@
-import React from "react";
+"use client";
+
+import { getProviders, signIn } from "next-auth/react";
+import { useState, useEffect } from "react";
+
+type Provider = {
+  id: string;
+  name: string;
+  type: string;
+  signInUrl: string;
+  callbackUrl: string;
+  signinUrlParams: Record<string, string> | undefined;
+};
+
+type Providers = Record<string, Provider>;
 
 const AuthProviders = () => {
-  return <div>AuthProviders</div>;
+  const [providers, setProviders] = useState<Providers | null>(null);
+
+  useEffect(() => {
+    const fetchProvider = async () => {
+      const res = await getProviders();
+      setProviders(res);
+    };
+
+    fetchProvider();
+  }, []);
+
+  if (providers) {
+    return (
+      <div>
+        {Object.values(providers).map((provider: Provider, idx) => (
+          <button key={idx} onClick={() => signIn()}>
+            {provider.id}
+          </button>
+        ))}
+      </div>
+    );
+  }
 };
 
 export default AuthProviders;
---
+++ lib/actions.ts
@@ -0,0 +1,16 @@
+import { GraphQLClient } from "graphql-request";
+
+const isProduction = process.env.NODE_ENV === "production";
+const apiUrl = isProduction
+  ? process.env.NEXT_PUBLIC_GRAFBASE_API_URL || ""
+  : "http://";
+
+const client = new GraphQLClient("apiUrl");
+
+const makeGraphQLRequest = async (query: string, variables = {}) => {
+  try {
+    // client request ...
+  } catch (error) {
+    throw error;
+  }
+};
